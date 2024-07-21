291649e33a526c52d4f544879bf37da7b2e90f42
Israe-jpg <israeguennouni6@gmail.com>
2024-07-21 11:51:57 -0700

    book

---
+++ oop/book_class.py
@@ -0,0 +1,12 @@
+class Book:
+    def __init__(self, title, author, year):
+        self.title = title
+        self.author = author
+        self.year = year
+    def __str__(self):
+        return f"{title} by {author}, published in {year}"
+    def __rep__(self):
+        return f"Book('{self.title}', '{self.author}', {self.year})"
+    def __del__(self):
+        return f"Deleting {self.title}"
+
